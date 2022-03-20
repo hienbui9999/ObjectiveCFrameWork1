4f64fdad4bc632d400797596e05a7a1c8fd86966
hienbui9999 <buiduyhien@gmail.com>
2022-03-20 08:51:59 +0700

    Initial Commit

---
+++ ObjectiveCFrameWork1/ObjectiveCFrameWork1.h
@@ -0,0 +1,18 @@
+//
+//  ObjectiveCFrameWork1.h
+//  ObjectiveCFrameWork1
+//
+//  Created by Hien on 20/03/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ObjectiveCFrameWork1.
+FOUNDATION_EXPORT double ObjectiveCFrameWork1VersionNumber;
+
+//! Project version string for ObjectiveCFrameWork1.
+FOUNDATION_EXPORT const unsigned char ObjectiveCFrameWork1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjectiveCFrameWork1/PublicHeader.h>
+
+
---
+++ ObjectiveCFrameWork1Tests/ObjectiveCFrameWork1Tests.m
@@ -0,0 +1,36 @@
+//
+//  ObjectiveCFrameWork1Tests.m
+//  ObjectiveCFrameWork1Tests
+//
+//  Created by Hien on 20/03/2022.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ObjectiveCFrameWork1Tests : XCTestCase
+
+@end
+
+@implementation ObjectiveCFrameWork1Tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
