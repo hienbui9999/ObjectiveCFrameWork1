87efba7454a0403a3c1cf1457b5a63b6fbd378f7
hienbui9999 <buiduyhien@gmail.com>
2022-03-20 09:09:01 +0700

    a

--- ObjectiveCFrameWork1Tests/ObjectiveCFrameWork1Tests.m
+++ ObjectiveCFrameWork1Tests/ObjectiveCFrameWork1Tests.m
@@ -27,10 +27,10 @@
 }
 
 - (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+    NSLog(@"-------------------Test performance done!---------------------");
+    NSLog(@"-------------------Test performance done!---------------------");
+    NSLog(@"-------------------Test performance done!---------------------");
+    NSLog(@"-------------------Test performance done!---------------------");
 }
 
 @end
